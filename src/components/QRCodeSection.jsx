eba72393c1683d2e8c55396ab04cdcbb2ea10692
VijayMakwana <vijay.makwana@tinyscript.in>
2026-01-28 10:11:58 +0530

    add qr code

--- src/components/QRCodeSection.jsx
+++ src/components/QRCodeSection.jsx
@@ -27,22 +27,22 @@ const QRCodeSection = () => {
         <p className="subtitle">
           Scan the QR code below to explore our products
         </p>
-        {/* <div className="qr-code-wrapper">
+        <div className="qr-code-wrapper">
           <QRCodeSVG
             value={websiteUrl}
             size={280}
             level="H"
             includeMargin={true}
           />
-        </div> */}
-        <div className="qr-code-wrapper">
+        </div>
+        {/* <div className="qr-code-wrapper">
           <img
             src={qrCodeImg}
             alt="Download Rebecca Overseas Catalog"
             width={285}
             height={280}
           />
-        </div>
+        </div> */}
         <p>Point your camera at the QR code to view our product catalog</p>
       </div>
     </section>
