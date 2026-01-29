7158cdb9a60251ff54fdbf44c65a06d4007ddb3b
Patel Sagar <patelsagar@Patels-MacBook-Air.local>
2026-01-29 15:13:38 +0530

    added icons and bg image change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { MdOutlineAttachEmail, MdPhone } from "react-icons/md";
+import Logo from "../assets/img/Logo.svg";
 import "./Header.css";
 
 const Header = () => {
@@ -7,12 +9,22 @@ const Header = () => {
       <div className="header-container">
         <div className="logo">
           <Link to="/">
-            <h1>Rebecca Overseas</h1>
+            <img src={Logo} alt="Rebecca OPverseas" className="logo-img w-[14rem] h-[50px]" />
           </Link>
         </div>
         <div className="contact-info">
-          <p>Email: rebeccaoverseascompany@gmail.com</p>
-          <p>Phone: +91 78 74 77 66 88</p>
+          <p className="flex items-center gap-2">
+            <MdOutlineAttachEmail className="text-lg" />
+            <a href="mailto:rebeccaoverseascompany@gmail.com" className="hover:underline">
+              rebeccaoverseascompany@gmail.com
+            </a>
+          </p>
+          <p className="flex items-center gap-2">
+            <MdPhone className="text-lg" />
+            <a href="tel:+917874776688" className="hover:underline">
+              +91 78 74 77 66 88
+            </a>
+          </p>
         </div>
       </div>
       <nav className="p-4 header-nav">
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,15 @@
 import './Hero.css';
+import vegiBackground from '../assets/images/Background/vegi.jpg';
 
 const Hero = () => {
     return (
-        <section className="hero">
-            <div className="hero-banner">
-                <h2>Download Product Brochures</h2>
+        <section className="hero relative">
+            <div
+                className="absolute inset-0 bg-center bg-cover bg-fixed sm:bg-fixed"
+                style={{ backgroundImage: `url(${vegiBackground})` }}
+            ></div>
+            <div className="hero-banner relative z-10">
+                <h2>Product Package of Practices</h2>
             </div>
         </section>
     );
